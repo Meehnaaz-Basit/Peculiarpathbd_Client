44c557d63b5fb68cf791547cd974c33dc5e4b1dd
Meehnaaz Basit <mhnzbasit.0002@gmail.com>
2024-09-08 13:15:12 +0600

    payment success page ui

---
+++ src/Dashboard/User/Booking/paymentSuccess/PaymentSuccess.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const PaymentSuccess = () => {
+  return (
+    <div className="flex justify-center items-center h-[45vh]">
+      <h1 className="text-4xl">Payment Successful</h1>
+    </div>
+  );
+};
+
+export default PaymentSuccess;
