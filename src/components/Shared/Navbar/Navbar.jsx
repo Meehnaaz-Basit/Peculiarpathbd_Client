f1f705107add9d221c85e78fcab98432b387a8db
Meehnaaz Basit <mhnzbasit.0002@gmail.com>
2024-09-08 15:52:08 +0600

    impliment dark mode

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 // import { AiOutlineMenu } from "react-icons/ai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 import useAuth from "../../../hooks/useAuth";
@@ -8,6 +8,27 @@ import Button from "../button/Button";
 const Navbar = () => {
   const { user, logOut } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
+  // theme
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme");
+    if (localTheme) {
+      setTheme(localTheme);
+      document.querySelector("html").setAttribute("data-theme", localTheme);
+    } else {
+      setTheme("light");
+      localStorage.setItem("theme", "light");
+    }
+  }, []);
+
+  const handleToggle = (e) => {
+    const newTheme = e.target.checked ? "night" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.querySelector("html").setAttribute("data-theme", newTheme);
+  };
+  // theme end
 
   const navRoute = (
     <>
@@ -243,6 +264,50 @@ const Navbar = () => {
             </>
           )}
         </div>
+        {/*  */}
+        <a id="clickable" className="z-30 ml-3">
+          {theme !== null && (
+            <label className="cursor-pointer grid place-items-center">
+              <input
+                onChange={handleToggle}
+                type="checkbox"
+                checked={theme === "night"}
+                // value="synthwave"
+                className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+              />
+              <svg
+                className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+                xmlns="http://www.w3.org/2000/svg"
+                width="10"
+                height="10"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="12" r="5" />
+                <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+              </svg>
+              <svg
+                className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+                xmlns="http://www.w3.org/2000/svg"
+                width="10"
+                height="10"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+              </svg>
+            </label>
+          )}
+        </a>
+        {/*  */}
       </div>
     </div>
   );
